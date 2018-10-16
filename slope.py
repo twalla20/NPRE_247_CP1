64c68e561b754e9d65c56192550c6d2b91106975
Tim Wallace <twalla20@illinois.edu>
2018-10-16 12:45:08 -0500

    Slight changes to slope.py

--- slope.py
+++ slope.py
@@ -1,3 +1,9 @@
 #
-This method makes use of forward finite differencing to find the slope of 
-a function ove some time interval - delta t.
+#This method makes use of forward finite differencing to find the slope of 
+#a function ove some time interval - delta t.
+#
+
+import os
+import math
+import numpy as np
+
