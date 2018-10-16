2ef58a505386314b3ddae1bcb36996678fc23f46
Tim Wallace <twalla20@illinois.edu>
2018-10-16 13:26:17 -0500

    Initialized a file that contains the class for the analytical solution of the problem

---
+++ analytSoln.py
@@ -0,0 +1,28 @@
+#author@ twalla20
+#
+#This file contants the method for solving the decay chain alaytically.
+##
+
+#Imports
+import os
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+#Class initialization for solution
+#
+#Args:
+#	halflife:	array size 3 containing half lives of all three nuclides
+#	initNum:	array size 3 containing initial number of nucleons for 
+#			for each nuclide
+#	dt:		size of time step
+#	tf:		total time elapsed
+#
+#Returns:
+#	atoms: array size 3 containg NAf, NBf and NCf
+
+Class AnalyticalDecaySolution:
+	def __init__(self, halfLife, initNum, dt, tf) :
+		
+	return atoms
---
+++ slope.md
@@ -0,0 +1,1 @@
+#This file was a mistake. To be deleted.
