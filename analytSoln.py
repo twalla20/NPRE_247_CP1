6c77dfd3a77487e12d61f8aa70f478b13c488551
Tim Wallace <twalla20@illinois.edu>
2018-10-16 19:33:49 -0500

    Removed dt paramenter from analytical soln. Converted input parameters from analytical solution to simple arrays. Implemented for loop to convery half-lives to lamda values.

--- analytSoln.py
+++ analytSoln.py
@@ -1,7 +1,7 @@
 #author@ twalla20
 #
 #This file contants the method for solving the decay chain alaytically.
-##
+#
 
 #Imports
 import os
@@ -16,13 +16,17 @@ import matplotlib.pyplot as plt
 #	halflife:	array size 3 containing half lives of all three nuclides
 #	initNum:	array size 3 containing initial number of nucleons for 
 #			for each nuclide
-#	dt:		size of time step
 #	tf:		total time elapsed
 #
 #Returns:
 #	atoms: array size 3 containg NAf, NBf and NCf
 
-Class AnalyticalDecaySolution:
-	def __init__(self, halfLife, initNum, dt, tf) :
-		
+Class AnalytDecaySolution:
+
+	def __init__(self, halfLife, initNum, tf):
+
+		lamda = halflife.copy()
+		for x in np.nditer(lamda, op_flags['readwrite']):
+			x[...] = (math.log(2))/x
+
 	return atoms
