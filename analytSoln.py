03ef9291295aed3b8156a13f373d907ad2ab2e32
Tim Wallace <twalla20@illinois.edu>
2018-10-21 14:09:11 -0500

    Made analytSoln a mudole instead of a class. First implementation. Have not tested.

--- analytSoln.py
+++ analytSoln.py
@@ -8,26 +8,36 @@
 import os
 import numpy as np
 import math
-from utilities import expDecay as expDecay
-from utilities import lamda as lamda
+from utilities import expDecay as eD
+from utilities import decayConst as dC
 
 
-#Class initialization for solution
+#Module initialization for analytical solution
 #
 #Args:
-#	halflife:	array size 3 containing half lives of all three nuclides
-#	initNum:	array size 3 containing initial number of nucleons for 
+#	hL:	array containing half lives of all three nuclides
+#	N0:	array containing initial number of nucleons for 
 #			for each nuclide
 #	tf:		total time elapsed
 #
 #Returns:
-#	atoms: array size 3 containg NAf, NBf and NCf
+#	atoms: array containg NAf, NBf and NCf
 
-Class AnalytDecaySolution:
+def analytSoln(hL, N0, tf):
+	atoms = []
+	lambda = dC(hL)
+	
+#Appends list with final number of parent nuclides
+	atoms.append(eD(hL[0]), N0[0], tf)
+ 
+#Appends list with final number of 1st daughter nuclides
+	Nd1 = eD(hL[1]) + (lambda[0]*N0[0]*(math.exp(-lambda[0]*tf) - math.exp(-lambda[1]*tf)))/(lambda[1]-lambda[0])
+	atoms.append(Nd1)
 
-	def __init__(self, halfLife, initNum, tf):
-		lamda = lamda(halfLife)
-		
-		
+#Appends list with final number of stable daughter nuclides	
+	Nd2 = N0[2] + N0[1](1 - math.exp(-lambda[1]*tf)) + (N0[0]*(lambda[1]*(1 - math.exp(-lambda[0]*tf)) - lambda[0]*(1 - math.exp(-lambda[1]*tf)))/(lambda[1] - lambda[0])
+	atoms.append(Nd2)
 
 	return atoms
+
+#End of analySoln module
