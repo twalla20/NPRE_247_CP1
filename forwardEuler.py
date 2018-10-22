c64015cb97ce504d7f16ebe2a2e5a6067ba3c493
Tim Wallace <twalla20@illinois.edu>
2018-10-21 19:41:04 -0500

    Re-commit to ensure I did not lose anything.

---
+++ forwardEuler.py
@@ -0,0 +1,12 @@
+#authro@ twalla20
+#
+#Module holds the forward euler method for the numerical solution
+#
+
+#Imports
+import math
+import numpy as np
+import matplotlib 
+import pyplot as plt
+
+
