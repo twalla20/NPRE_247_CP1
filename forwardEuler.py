0a02dda161e419418f88e95b9965456f0dff310c
Tim Wallace <twalla20@illinois.edu>
2018-10-23 18:21:15 -0500

    Functions now modify the list of original atoms

--- forwardEuler.py
+++ forwardEuler.py
@@ -10,7 +10,7 @@ from utilities import expDecay as eD
 from utilities import decayConst as dC
 
 
-def fwdEu(tf, dt, i, L, N0):
+def fwdEu(tf, dt, i, hL, N0):
 	"""
 	First Order ODE solver function using Forward Eurler's method for numerical solution
 
@@ -19,10 +19,9 @@ def fwdEu(tf, dt, i, L, N0):
 	"""
 	if dt <= 0:
 		return [0]
-
 	if i==0:
 		f = NA
-	else if i==1:
+	if i==1:
 		f = NB
 	else:
 		f = NC
@@ -34,12 +33,12 @@ def fwdEu(tf, dt, i, L, N0):
 	x = 0
 	for j in range(n):
 		y += dt * f(L, N0, x)
+		x +=  dt * (n+1)
 		atoms.append(y)
-		x += dt * (n+1)
 	return atoms
 # end of Forward Euler's method
 
-def NA(L=[], N0=[], x=0):
+def NA(L, N0, x):
 	"""
 	First Order ODE function for parent nuclide decay
 	
@@ -51,10 +50,12 @@ def NA(L=[], N0=[], x=0):
 	Returns:
 		NA'(x)
 	"""
-	return round(-L[0] * eD(L[0], N0[0], x))
+	a =  round(-L[0] * eD(L[0], N0[0], x))
+	N0[0] = N0[0] - a
+	return m
 # end of parent function
 
-def NB(L=[] N0=[], x=0):
+def NB(L, N0, x):
 	"""
         First Order ODE function for 1st daughter 
 	nuclide decay/production
@@ -69,10 +70,12 @@ def NB(L=[] N0=[], x=0):
         """
 	a = -L[1]*(eD(L[1], N0[1], x) + (L[0]*N0[0]*(math.exp(-L[0]*x) - math.exp(-L[1]*x)))/(L[1]-L[0]))
 	b = L[0]*eD(L[0], No[0], x)
+	N0[0] = N0[0] - b
+	N0[1] = N0[1] + a
 	return round(a+b)
 # end of first daughter function
 
-def NC(L=[], N0=[], x=0):
+def NC(L, N0, x):
 	"""
         First Order ODE function for 2nd daughter 
 	(stable) nuclide production
@@ -86,5 +89,6 @@ def NC(L=[], N0=[], x=0):
                 NC'(x)
         """
 	a = L[1]*(eD(L[1], N0[1], x) + (L[0]*N0[0]*(math.exp(-L[0]*x) - math.exp(-L[1]*x)))/(L[1]-L[0]))
+	N0[2] = N0[2] + a
 	return round(a)
 # end of stable daughter function
